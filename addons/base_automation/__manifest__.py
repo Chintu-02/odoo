ca41f26294619f67c32bd4d3977024ea156d3a10
Chanchal Gharpande <105722887+Chintu-02@users.noreply.github.com>
2024-09-19 09:57:36 +0530

    Updat__manifest__.py

--- addons/base_automation/__manifest__.py
+++ addons/base_automation/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Automation Rules',
-    'version': '1.4',
+    'version': '1.5',
     'category': 'Sales/Sales',
     'description': """
 This module allows to implement automation rules for any object.
